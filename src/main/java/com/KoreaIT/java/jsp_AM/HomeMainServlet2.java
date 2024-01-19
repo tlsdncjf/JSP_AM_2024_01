7576f0e3aa8eb9d53b5bd5821f178b039d7926ae
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-01-19 17:14:19 +0900

    작업 4, 구구단 기초, 파라미터 사용

--- src/main/java/com/KoreaIT/java/jsp_AM/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/jsp_AM/HomeMainServlet2.java
@@ -1,25 +1,38 @@
 package com.KoreaIT.java.jsp_AM;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet("/printDan")
 public class HomeMainServlet2 extends HttpServlet {
-	
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		response.setContentType("text/html;charset=UTF-8");	
 
-		response.getWriter().append("==8단==<br>");
-		
-		int dan = 8;
-		
-		for (int i = 1; i <= 9; i++) {
+		response.setContentType("text/html;charset=UTF-8");
+
+		String inputedDan = request.getParameter("dan");
+		String inputedLimit = request.getParameter("limit");
+
+		if (inputedDan == null) {
+			inputedDan = "1";
+		}
+		if (inputedLimit == null) {
+			inputedLimit = "1";
+		}
+
+
+		int dan = Integer.parseInt(inputedDan);
+		int limit = Integer.parseInt(inputedLimit);
+
+		response.getWriter().append(String.format("==%d단==<br>", dan));
+
+		for (int i = 1; i <= limit; i++) {
 			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
 		}
 	}

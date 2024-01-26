fbaca94e3eb1a99510fbdf7b220198315a3609b3
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-01-26 11:11:16 +0900

    작업 31, 디스패처 서블릿 도입, 프로그램의 입구 감소

---
+++ src/main/java/com/KoreaIT/java/jsp_AM/servlet/DispatcherServlet.java
@@ -0,0 +1,101 @@
+package com.KoreaIT.java.jsp_AM.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+import com.KoreaIT.java.jsp_AM.config.Config;
+import com.KoreaIT.java.jsp_AM.controller.ArticleController;
+import com.KoreaIT.java.jsp_AM.exception.SQLErrorException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/s/*")
+public class DispatcherServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+
+		// DB연결
+		Connection conn = null;
+		try {
+			Class.forName(Config.getDbDriverClassName());
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스가 없습니다.");
+			e.printStackTrace();
+			return;
+		}
+
+		try {
+			conn = DriverManager.getConnection(Config.getDbUrl(), Config.getDbUser(), Config.getDbPw());
+
+			// 모든 요청 응답 전 실행
+			boolean isLogined = false;
+			int loginedMemberId = -1;
+			Map<String, Object> loginedMember = null;
+
+			HttpSession session = request.getSession();
+
+			if (session.getAttribute("loginedMemberId") != null) {
+				isLogined = true;
+				loginedMemberId = (int) session.getAttribute("loginedMemberId");
+				loginedMember = (Map<String, Object>) session.getAttribute("loginedMember");
+			}
+
+			request.setAttribute("isLogined", isLogined);
+			request.setAttribute("loginedMemberId", loginedMemberId);
+			request.setAttribute("loginedMember", loginedMember);
+
+			String requestUri = request.getRequestURI();
+
+			System.out.println(requestUri);
+
+			String[] requestUriBits = requestUri.split("/");
+			// ~~/s/article/list
+			// [0][1] [2] [3]
+
+			if (requestUriBits.length < 5) {
+				response.getWriter().append("올바른 요청이 아닙니다.");
+				return;
+			}
+
+			String controllerName = requestUriBits[3];
+			String actionMethodName = requestUriBits[4];
+
+			if (controllerName.equals("article")) {
+				ArticleController articleController = new ArticleController(request, response, conn);
+
+				if (actionMethodName.equals("list")) {
+					articleController.showList();
+				}
+			}
+
+		} catch (SQLException e) {
+			System.out.println("에러 : " + e);
+		} catch (SQLErrorException e) {
+			e.getOrigin().printStackTrace();
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
